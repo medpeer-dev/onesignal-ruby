e522a9c3e77d72d35d6715161a62aa216cb1e5a2
櫻井 貴央 <takahisa.sakurai@pc-12-361.local>
2019-06-24 15:28:48 +0900

    fix badge class how to declare const

--- lib/onesignal/badge.rb
+++ lib/onesignal/badge.rb
@@ -1,13 +1,13 @@
 # frozen_string_literal: true
 
 module OneSignal
-  module IOSBadgeType
-    NONE = 'None'
-    SET_TO = 'SetTo'
-    INCREASE = 'Increase'
-  end
-
   class Badge
+    class IOSBadgeType
+      NONE = 'None'
+      SET_TO = 'SetTo'
+      INCREASE = 'Increase'
+    end
+
     attr_reader :ios_badgeType, :ios_badgeCount
 
     def initialize ios_badgeType: nil, ios_badgeCount: nil
